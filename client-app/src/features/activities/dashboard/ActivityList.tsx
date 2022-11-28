e616f966b85e04dc64d9c061dda81f85be89bb05
leonharf <joh.leonhardt@anz.com>
2022-11-28 12:56:59 +1100

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,13 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
 
 export default observer(function ActivityList() {
 
-  const {activityStore} = useStore();
-  const {deleteActivity, activitiesByDate, loading} =activityStore;
+  const { activityStore } = useStore();
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
   const [target, setTarget] = useState('');
 
   function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
@@ -29,21 +30,22 @@ export default observer(function ActivityList() {
                 <div>{activity.city}, {activity.venue}</div>
               </Item.Description>
               <Item.Extra>
-                <Button 
-                  onClick={() => activityStore.selectActivity(activity.id)} 
-                  floated='right' 
-                  content='View' 
-                  color='blue' 
+                <Button
+                  as={Link}
+                  to={`/activities/${activity.id}`}
+                  floated='right'
+                  content='View'
+                  color='blue'
                 />
-                <Button 
+                <Button
                   name={activity.id}
-                  loading={loading && target === activity.id} 
-                  onClick={(e) => handleActivityDelete(e, activity.id)} 
-                  floated='right' 
-                  content='Delete' 
-                  color='red' 
+                  loading={loading && target === activity.id}
+                  onClick={(e) => handleActivityDelete(e, activity.id)}
+                  floated='right'
+                  content='Delete'
+                  color='red'
                 />
-                <Label basic content={activity.category}/>
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
