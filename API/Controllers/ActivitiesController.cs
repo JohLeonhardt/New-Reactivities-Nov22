73f31d7254f088ca443e1918668d32d7f315330f
leonharf <joh.leonhardt@anz.com>
2022-12-07 16:00:16 +1100

    Section 13 - 145. Creating a login form

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,15 +1,14 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
-using System.Threading;
 using Domain;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace API.Controllers
 {
+  [AllowAnonymous]
   public class ActivitiesController : BaseApiController
   {
 
