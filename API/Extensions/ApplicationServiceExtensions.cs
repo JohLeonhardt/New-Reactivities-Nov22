b876f9465702d6bafcd958394197711528838929
leonharf <joh.leonhardt@anz.com>
2022-12-08 16:13:03 +1100

    Section 14 - 159. Updating the create activity handler

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,45 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Application.Activities;
 using Application.Core;
 using Application.Interfaces;
-using FluentValidation;
-using FluentValidation.AspNetCore;
+using AutoMapper;
 using Infrastructure.Security;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Persistence;
 namespace API.Extensions
 {
-  public static class ApplicationServiceExtensions
-  {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
-        IConfiguration config)
+    public static class ApplicationServiceExtensions
     {
-      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-      services.AddEndpointsApiExplorer();
-      services.AddSwaggerGen();
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-      });
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
-              {
-            policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-          });
-      });
-      services.AddMediatR(typeof(List.Handler));
-      services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-      services.AddFluentValidationAutoValidation();
-      services.AddValidatorsFromAssemblyContaining<Create>();
-      services.AddHttpContextAccessor();
-      services.AddScoped<IUserAccessor, UserAccessor>();
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(opt => 
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(new string[] {"http://localhost:3000", "test"});
+                });
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddScoped<IUserAccessor, UserAccessor>();
 
-      return services;
+            return services;
+        }
     }
-  }
 }
