2f77b74ae6fd46ee72c85dc6155edea48457edbb
leonharf <joh.leonhardt@anz.com>
2022-12-08 18:36:14 +1100

    Section 14 - 165. Add a custom auth policy

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,43 +1,51 @@
 using System.Text;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
-
 namespace API.Extensions
 {
-    public static class IdentityServiceExtensions
+  public static class IdentityServiceExtensions
+  {
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
+        IConfiguration config)
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-            IConfiguration config)
-        {
-            services.AddIdentityCore<AppUser>(opt =>
+      services.AddIdentityCore<AppUser>(opt =>
+      {
+        opt.Password.RequireNonAlphanumeric = false;
+      })
+      .AddEntityFrameworkStores<DataContext>()
+      .AddSignInManager<SignInManager<AppUser>>();
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(opt =>
+          {
+            opt.TokenValidationParameters = new TokenValidationParameters
             {
-                opt.Password.RequireNonAlphanumeric = false;
-            })
-            .AddEntityFrameworkStores<DataContext>()
-            .AddSignInManager<SignInManager<AppUser>>();
+              ValidateIssuerSigningKey = true,
+              IssuerSigningKey = key,
+              ValidateIssuer = false,
+              ValidateAudience = false
+            };
+          });
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+      services.AddAuthorization(opt =>
+      {
+        opt.AddPolicy("IsActivityHost", policy =>
+              {
+            policy.Requirements.Add(new IsHostRequirement());
+          });
+      });
+      services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+      services.AddScoped<TokenService>();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(opt =>
-                {
-                    opt.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = key,
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
-            services.AddScoped<TokenService>();
-
-            return services;
-        }
+      return services;
     }
+  }
 }
