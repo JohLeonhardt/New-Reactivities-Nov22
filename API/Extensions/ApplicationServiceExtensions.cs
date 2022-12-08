06c1539038e5b68f6f088d009cfa11021927394b
leonharf <joh.leonhardt@anz.com>
2022-12-08 13:18:23 +1100

    Not working - Section 14 - 159. Adding an infratructure project

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,43 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using Persistence;
-using AutoMapper;
 using Application.Activities;
 using Application.Core;
-
+using Application.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Infrastructure.Security;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtensions
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddSwaggerGen(c =>
-            {
-                // c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      services.AddEndpointsApiExplorer();
+      services.AddSwaggerGen();
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+              {
+            policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+          });
+      });
+      services.AddMediatR(typeof(List.Handler));
+      services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+      services.AddFluentValidationAutoValidation();
+      services.AddValidatorsFromAssemblyContaining<Create>();
+      services.AddHttpContextAccessor();
+      services.AddScoped<IUserAccessor, UserAccessor>();
 
-            services.AddCors(opt => 
-            {
-                opt.AddPolicy("CorsPolicy", policy => 
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
-            return services;
-        }
+      return services;
     }
+  }
 }
