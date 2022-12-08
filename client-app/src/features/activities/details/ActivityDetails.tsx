b0a32ac24e9126077979d81e34837756bc075190
leonharf <joh.leonhardt@anz.com>
2022-12-09 10:33:52 +1100

    Section 14 - 171. Updating the details component

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -29,32 +29,9 @@ export default observer(function ActivityDetails() {
         <ActivityDetailedInfo activity={activity}/>
         <ActivityDetailedChat />
         <Grid.Column width={6}>
-          <ActivityDetailedSidebar />
+          <ActivityDetailedSidebar attendees={activity.attendees!}/>
         </Grid.Column>
       </Grid.Column>
     </Grid>
   )
 })
-
-
-
-
-
-// <Card fluid>
-//       <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
-//       <Card.Content>
-//         <Card.Header>{activity.title}</Card.Header>
-//         <Card.Meta>
-//           <span>{activity.date}</span>
-//         </Card.Meta>
-//         <Card.Description>
-//           {activity.description}
-//         </Card.Description>
-//       </Card.Content>
-//       <Card.Content extra>
-//         <Button.Group widths='2'>
-//           <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
-//           <Button as={Link} to='/activities' basic color='grey' content='Cancel' />
-//         </Button.Group>
-//       </Card.Content>
-//     </Card>
