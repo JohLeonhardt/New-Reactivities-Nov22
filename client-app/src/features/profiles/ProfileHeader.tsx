7631f96d9c4248addd4300e9577b90d7a93787b4
leonharf <joh.leonhardt@anz.com>
2022-12-15 16:00:46 +1100

    Section 17 - 196. Getting the profile data part 2

--- client-app/src/features/profiles/ProfileHeader.tsx
+++ client-app/src/features/profiles/ProfileHeader.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 import { Grid, Segment, Item, Header, Statistic, Divider, Reveal, Button } from "semantic-ui-react";
 import { Profile } from '../../app/models/profile';
 
-export default function ProfileHeader() {
+
+interface Props {
+  profile: Profile;
+}
+
+export default observer(function ProfileHeader({profile}: Props) {
     return (
         <Segment>
             <Grid>
@@ -13,10 +18,10 @@ export default function ProfileHeader() {
                             <Item.Image
                                 avatar
                                 size='small'
-                                src={'/assets/user.png'}
+                                src={profile.image || '/assets/user.png'}
                             />
                             <Item.Content verticalAlign='middle'>
-                                <Header as='h1' content='Displayname'/>
+                                <Header as='h1' content={profile.displayName}/>
                             </Item.Content>
                         </Item>
                     </Item.Group>
@@ -49,4 +54,4 @@ export default function ProfileHeader() {
         </Segment>
 
     )
-}
+})
