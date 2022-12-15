d676b7d266c0662e4b406025362245063a466803
leonharf <joh.leonhardt@anz.com>
2022-12-15 15:00:30 +1100

    Section 17 - 192. Creating a profile page (Had to stop following Reactivities-v6 and change everything over to Reactivities repo as Neil changed course mid way and updated everything)

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,62 +1,41 @@
-import React, { useEffect } from 'react';
-import { Container, Modal } from 'semantic-ui-react';
-import Navbar from './Navbar';
+import { Container } from 'semantic-ui-react';
+import NavBar from './Navbar';
 import { observer } from 'mobx-react-lite';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import TestErrors from '../../features/errors/TestError';
 import { ToastContainer } from 'react-toastify';
-import NotFound from '../../features/errors/NotFound';
-import ServerError from '../../features/errors/ServerError';
-import LoginForm from '../../features/users/LoginForm';
 import { useStore } from '../stores/store';
+import { useEffect } from 'react';
 import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modals/ModalContainer';
 
 function App() {
-
   const location = useLocation();
-  const {commonStore, userStore} = useStore();
+  const { commonStore, userStore } = useStore();
 
   useEffect(() => {
     if (commonStore.token) {
-      userStore.getUser().finally(() => commonStore.setAppLoaded());
+      userStore.getUser().finally(() => commonStore.setAppLoaded())
     } else {
-      commonStore.setAppLoaded();
+      commonStore.setAppLoaded()
     }
-  }, [commonStore, userStore]);
+  }, [commonStore, userStore])
 
   if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
 
   return (
     <>
-      <ToastContainer position='bottom-right' hideProgressBar />
       <ModalContainer />
-      <Route exact path='/' component={HomePage} />
-      <Route
-        path={'/(.+)'}
-        render={() => (
-          <>
-            <Navbar />
-            <Container style={{ marginTop: '7em' }}>
-              <Switch>
-                <Route exact path='/activities' component={ActivityDashboard} />
-                <Route path='/activities/:id' component={ActivityDetails} />
-                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                <Route path='/errors' component={TestErrors} />
-                <Route path='/server-error' component={ServerError} />
-                <Route path='/login' component={LoginForm} />
-                <Route component={NotFound} />
-              </Switch>
-            </Container>
-          </>
-        )}
-      />
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+      {location.pathname === '/' ? <HomePage /> : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: '7em' }}>
+            <Outlet />
+          </Container>
+        </>
+      )}
     </>
   );
 }
-
 export default observer(App);
--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { Button, Container, Dropdown, Image, Menu } from 'semantic-ui-react';
 import { useStore } from '../stores/store';
 
-export default observer(function Navbar() {
+export default observer(function NavBar() {
   const { userStore: { user, logout } } = useStore();
   return (
     <Menu inverted fixed='top'>
@@ -22,7 +22,7 @@ export default observer(function Navbar() {
           <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
           <Dropdown pointing='top left' text={user?.displayName}>
             <Dropdown.Menu>
-              <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user' />
+              <Dropdown.Item as={Link} to={`/profiles/${user?.username}`} text='My Profile' icon='user' />
               <Dropdown.Item onClick={logout} text='Logout' incon='power' />
             </Dropdown.Menu>
           </Dropdown>
