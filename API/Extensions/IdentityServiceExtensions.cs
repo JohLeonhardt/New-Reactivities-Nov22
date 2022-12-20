d8ab89f48fa399dd5af83f914a3727c20ab54d58
leonharf <joh.leonhardt@anz.com>
2022-12-20 11:39:29 +1100

    Section 19 - 214. Authenticating to SignalR (Code broken in Program.cs from Lecture 213

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Threading.Tasks;
 using API.Services;
 using Domain;
 using Infrastructure.Security;
@@ -22,7 +23,9 @@ namespace API.Extensions
       })
       .AddEntityFrameworkStores<DataContext>()
       .AddSignInManager<SignInManager<AppUser>>();
+
       var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+
       services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
           .AddJwtBearer(opt =>
           {
@@ -32,6 +35,19 @@ namespace API.Extensions
               IssuerSigningKey = key,
               ValidateIssuer = false,
               ValidateAudience = false
+            };
+            opt.Events = new JwtBearerEvents
+            {
+              OnMessageReceived = context =>
+              {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/chat")))
+                {
+                  context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+              }
             };
           });
 
