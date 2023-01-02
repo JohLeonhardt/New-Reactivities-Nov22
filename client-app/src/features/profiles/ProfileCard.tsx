f1ce79dc251fdb153144af290570d8bf57869d7d
leonharf <joh.leonhardt@anz.com>
2023-01-02 14:27:43 +1100

    Section 20 - Lecture 231. Making the follow button a compnent

--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -3,6 +3,7 @@ import { Card, Icon, Image } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
+import FollowButton from './FollowButton';
 
 interface Props {
     profile: Profile
@@ -20,14 +21,13 @@ export default observer(function ProfileCard({ profile }: Props) {
             <Image src={profile.image || '/assets/user.png'} />
             <Card.Content>
                 <Card.Header>{profile.displayName}</Card.Header>
-                <Card.Description>
-                    {truncate(profile.bio)}
-                </Card.Description>
+                <Card.Description>{truncate(profile.bio)}</Card.Description>
             </Card.Content>
             <Card.Content extra>
                 <Icon name='user' />
                 {profile.followersCount} followers
             </Card.Content>
+            <FollowButton profile={profile}/>
         </Card>
     )
 })
