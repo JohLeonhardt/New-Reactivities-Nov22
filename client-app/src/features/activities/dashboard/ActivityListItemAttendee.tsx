0d93f9f840b4598a2a71e8c10209b7cfcaaff0a1
leonharf <joh.leonhardt@anz.com>
2023-01-02 13:44:51 +1100

    Section 20 - Lecture 229. Adding methods to follow and unfollow

--- client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -4,10 +4,18 @@ import {observer} from "mobx-react-lite";
 import { Profile } from '../../../app/models/profile';
 import { Link } from 'react-router-dom';
 import ProfileCard from '../../profiles/ProfileCard';
+
 interface Props {
     attendees: Profile[];
 }
+
 export default observer(function ActivityListItemAttendee({attendees}: Props) {
+
+    const styles = {
+        borderColor: 'orange',
+        borderWidth: 2
+    }
+
     return (
         <List horizontal>
             {attendees.map(attendee => (
@@ -16,9 +24,13 @@ export default observer(function ActivityListItemAttendee({attendees}: Props) {
                     key={attendee.username}
                     trigger={
                         <List.Item as={Link} to={`/profiles/${attendee.username}`}>
-                            <Image size='mini'
-                                   circular
-                                   src={attendee.image || `/assets/user.png`} />
+                            <Image 
+                                size='mini'
+                                circular
+                                src={attendee.image || `/assets/user.png`}
+                                bordered
+                                style={attendee.following ? styles : null}
+                             />
                         </List.Item>
                     }
                 >
