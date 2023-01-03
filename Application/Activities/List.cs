bb5066998811880aca1ec1debbb7744b00da184a
leonharf <joh.leonhardt@anz.com>
2023-01-03 16:13:39 +1100

    Section 21 - Lecture 238. Adding a pagination header

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -33,14 +33,15 @@ namespace Application.Activities
       public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
       {
         var query = _context.Activities
-            .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
-              new { currentUsername = _userAccessor.GetUsername() })
-            .AsQueryable();
+          .OrderBy(d => d.Date)
+          .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
+            new { currentUsername = _userAccessor.GetUsername() })
+          .AsQueryable();
 
         return Result<PagedList<ActivityDto>>.Success(
           await PagedList<ActivityDto>.CreateAsync(query,
-                request.Params.PageNumber, request.Params.PageSize)
-                );
+            request.Params.PageNumber, request.Params.PageSize)
+        );
       }
     }
   }
