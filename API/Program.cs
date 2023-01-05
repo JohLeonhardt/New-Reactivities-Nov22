81f0f5550d11f88e86dfdf1e02baf56f7b2324cb
leonharf <joh.leonhardt@anz.com>
2023-01-05 18:40:03 +1100

    Section 22 - Lecture 268. Security headers added

--- API/Program.cs
+++ API/Program.cs
@@ -32,11 +32,11 @@ app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
 app.UseCspReportOnly(opt => opt 
   .BlockAllMixedContent()
-  .StyleSources(s => s.Self())
-  .FontSources(s => s.Self())
+  .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+  .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
   .FormActions(s => s.Self())
   .FrameAncestors(s => s.Self())
-  .ImageSources(s => s.Self())
+  .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
   .ScriptSources(s => s.Self())
 );
 
@@ -44,6 +44,14 @@ if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI();
+}
+else
+{
+  app.Use(async (context, next) =>
+  {
+    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+    await next.Invoke();
+  });
 }
 
 app.UseCors("CorsPolicy");
